a9324571064c8dc9e354cc63e6f1aa6d5bba42d9
AdrianNunez <adrianwakefiss@gmail.com>
2015-01-10 12:57:36 +0100

    Initial report

--- phone/platforms/android/ant-build/AndroidManifest.xml.d
+++ phone/platforms/android/ant-build/AndroidManifest.xml.d
@@ -1,2 +1,2 @@
-C:\Users\Adrian Núñez\Desktop\TFG\exerClick\phone\platforms\android\ant-build\AndroidManifest.xml : \
-C:\Users\Adrian Núñez\Desktop\TFG\exerClick\phone\platforms\android\AndroidManifest.xml \
+C:\Users\Adrian Núñez\Desktop\exerclick\exerClick\phone\platforms\android\ant-build\AndroidManifest.xml : \
+C:\Users\Adrian Núñez\Desktop\exerclick\exerClick\phone\platforms\android\AndroidManifest.xml \
